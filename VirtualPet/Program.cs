7364526c0bbe7986aef76017a2a7e3816c1ee848
Luis Cabrera <loudigi@gmail.com>
2017-02-23 01:14:20 -0500

    Upload first set of project files

---
+++ VirtualPet/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 20;
+            Console.Write(String.Format("{0,-10}","Hunger " + counter));
+            for (int i = 0; i < counter; i++){
+                Console.Write("|");
+            }
+            Console.WriteLine();
+
+            int counter2 = 8;
+            Console.Write(String.Format("{0,-10}", "Life " + counter2));
+            for (int i = 0; i < counter2; i++)
+            {
+                Console.Write("|");
+            }
+            Console.WriteLine();
+        }
+        
+    }
+    
+}
---
+++ VirtualPet/VirtualPet.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    class VirtualPet
+    {
+        private string petName;
+        private int petHunger;
+        private int petBoredom;
+        private int petTired;
+        private int petHappiness;
+
+
+
+    }
+}
